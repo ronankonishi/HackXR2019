217c6c53196dcd87821396bb109602fe933ad2bc
ManitKSoni <manitnsoni@gmai.com>
2019-05-05 04:11:44 -0700

    finished object scripts

---
+++ Assets/John Lemon Assets/Scripts/PlayerController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private HealthBar mHealthBar;
+    private HealthBar mFoodBar;
+    private int startHealth;
+    private int startFood;
+
+    public float turnSpeed = 20f;
+
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    GameObject Bars_Panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        mHealthBar = Bars_Panel.transform.Find("Bars_Panel/HealthBar").GetComponent<HealthBar>();
+        mHealthBar.Min = 0;
+        mHealthBar.Max = Health;
+        startHealth = Health;
+        mHealthBar.SetValue(Health);
+
+        mFoodBar = Bars_Panel.transform.Find("Bars_Panel/FoodBar").GetComponent<HealthBar>();
+        mFoodBar.Min = 0;
+        mFoodBar.Max = Food;
+        startFood = Food;
+        mFoodBar.SetValue(Food);
+
+        InvokeRepeating("IncreaseHunger", 0, HungerRate);
+    }
+
+    [Tooltip("Amount of health")]
+    public int Health = 100;
+
+    [Tooltip("Amount of food")]
+    public int Food = 100;
+
+    [Tooltip("Rate in seconds in which the hunger increases")]
+    public float HungerRate = 0.5f;
+
+    public void IncreaseHunger()
+    {
+        Food--;
+        if (Food < 0)
+            Food = 0;
+
+        mFoodBar.SetValue(Food);
+
+        if (IsDead)
+        {
+            CancelInvoke();
+        }
+    }
+
+    public bool IsDead
+    {
+        get
+        {
+            return Health == 0 || Food == 0;
+        }
+    }
+
+    public void Eat(int amount)
+    {
+        Food += amount;
+        if (Food > startFood)
+        {
+            Food = startFood;
+        }
+
+        mFoodBar.SetValue(Food);
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health < 0)
+        {
+            Health = 0;
+        }
+        mHealthBar.SetValue(Health);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, 
+        turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * 
+        m_Animator.deltaPosition.magnitude);
+
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+}
---
+++ Assets/LowPoly HUD/Scripts/Food.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public int FoodPoints = 20;
+
+    public void OnUse()
+    {
+        GameManager.Instance.Player.Eat(FoodPoints);
+
+        Destroy(gameObject);
+    }
+}
